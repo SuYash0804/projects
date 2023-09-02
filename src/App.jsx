4ccbc03992e33d1c56b8b165495d005ea2586a18
SuyashJ1520 <121536098+SuyashJ1520@users.noreply.github.com>
2023-09-03 01:17:58 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ export default function App() {
   const [task, setTask] = useState("")
   const initial = [{ time: moment(new Date()).format("MMMM Do YYYY, h:mm:ss a"), taskName: "Example task", lastEdited: "" }]
 
-  const {index, currState: tasks, updateState: setTasks, goBack, goForward, lastIndex} = UndoRedo({initial})
+  const { index, currState: tasks, updateState: setTasks, goBack, goForward, lastIndex } = UndoRedo({ initial })
 
   const canUndo = index > 0
   const canRedo = index < lastIndex
-
-  const [editKey, setEditKey] = useState(null)
   const [dragStartIndex, setDragStartIndex] = useState(null)
 
   function updateTasks() {
@@ -23,11 +21,12 @@ export default function App() {
     setTask("")
   }
 
-  function dragStart(key) {
+  function dragStart(e, key) {
     setDragStartIndex(key)
-    // const dragImage = e.target.cloneNode(true)
-    // dragImage.style.opacity = "1"
-    // e.dataTrasfer.setDragImage(dragImage)
+    const dragImage = e.target.cloneNode(true)
+    dragImage.style.opacity = "1"
+    dragImage.style.scale = "1.25"
+    e.dataTrasfer.setDragImage(dragImage)
   }
 
   function dragOver(e, key) {
@@ -46,59 +45,61 @@ export default function App() {
     setDragStartIndex(null)
   }
   return (
-    <div className="d-flex flex-column align-items-stretch p-2">
-      <div className="d-flex justify-content-center align-items-center p-4 gap-3">
-        <input value={task} onChange={e => setTask(e.target.value)} onKeyDown={e => {
-          if (e.key === "Enter") {
-            updateTasks()
-          }
-        }} id="enterTask" className="form-control w-auto" type="text" /><button onClick={updateTasks} className="btn border border-white text-white">Add Task</button>
-        <div className="d-flex gap-2 align-items-center">
-          <button onClick={goBack} disabled={!canUndo} className="btn btn-dark border border-white d-flex justify-content-center align-items-center">
-            <RotateCcw size={13.5} color="white" />
-          </button>
-          <button onClick={goForward} disabled={!canRedo} className="btn btn-dark border border-white d-flex justify-content-center align-items-center">
-            <RotateCw size={13.5} color="white" />
-          </button>
+    <>
+    <style>{`
+      .cursor-pointer {
+        cursor: pointer !inportant;
+      }
+    `}</style>
+      <div className="d-flex flex-column align-items-stretch p-2">
+        <div className="d-flex justify-content-center align-items-center p-4 gap-3">
+          <input value={task} onChange={e => setTask(e.target.value)} onKeyDown={e => {
+            if (e.key === "Enter") {
+              updateTasks()
+            }
+          }} id="enterTask" className="form-control w-auto" type="text" /><button onClick={updateTasks} className="btn border border-white text-white">Add Task</button>
+          <div className="d-flex gap-2 align-items-center">
+            <button onClick={goBack} disabled={!canUndo} className="btn btn-dark border border-white d-flex justify-content-center align-items-center">
+              <RotateCcw size={13.5} color="white" />
+            </button>
+            <button onClick={goForward} disabled={!canRedo} className="btn btn-dark border border-white d-flex justify-content-center align-items-center">
+              <RotateCw size={13.5} color="white" />
+            </button>
+          </div>
+        </div>
+        <div onDragOver={e => e.preventDefault()} className="p-3 m-auto border border-white flex-grow-1" style={{ width: "100%", maxWidth: "800px" }}>
+          <div className="d-flex flex-column align-items-stretch gap-3 h-100" style={{ maxHeight: "100%", overflow: "auto" }}>
+            {tasks.length >= 1 ? tasks.map((ele, key) => {
+              return (
+                <Card draggable={tasks.length > 1} onDragStart={(e) => dragStart(e, key)}
+                  onTouchStart={(e) => dragStart(e, key)}
+                  onTouchMove={e => dragOver(e, key)}
+                  onDragOver={e => dragOver(e, key)}
+                  onDragEnd={() => dragEnd()}
+                  onTouchEnd={() => dragEnd()}
+                  key={key} style={key === dragStartIndex ? { opacity: `0.5` } : {}}>
+                  <CardBody className="d-flex align-items-center position-relative gap-2">
+                    <div className="flex-grow-1">
+                      <span className="fs-5" onDragStart={e => e.stopPropagation}>{ele.taskName}</span>
+                    </div>
+                    <div className="d-flex align-items-center gap-3">
+                      <Edit className="cursor-pointer" size={17.5} /> <Trash className="cursor-pointer" onClick={() => {
+                        const arr = [...tasks]
+                        arr.splice(key, 1)
+                        setTasks([...arr])
+                      }} size={17.5} />
+                    </div>
+                    <span className="position-absolute cursor-pointer" style={{ inset: "0px 0px auto auto", fontSize: "10px", padding: "0.15rem" }} title={`Created at ${ele.time}${ele.lastEdited === "" ? "" : `, Last edited on ${ele.lastEdited}`}`}><Info size={12} /></span>
+                  </CardBody>
+                </Card>
+              )
+            }) : (
+              <label htmlFor="enterTask" className="text-white fs-5 text-center w-100">Enter a task</label>
+            )}
+          </div>
         </div>
       </div>
-      <div onDragOver={e => e.preventDefault()} className="p-3 m-auto border border-white flex-grow-1" style={{ width: "100%", maxWidth: "800px" }}>
-        <div className="d-flex flex-column align-items-stretch gap-3 h-100" style={{ maxHeight: "100%", overflow: "auto" }}>
-          {tasks.length >= 1 ? tasks.map((ele, key) => {
-            return (
-              <Card draggable={tasks.length > 1} onDragStart={(e) => dragStart(key)}
-                onTouchStart={(e) => dragStart(key)}
-                onTouchMove={e => dragOver(e, key)}
-                onDragOver={e => dragOver(e, key)}
-                onDragEnd={() => dragEnd()}
-                onTouchEnd={() => dragEnd()}
-                key={key} style={key === dragStartIndex ? { opacity: `0.5` } : {}}>
-                <CardBody className="d-flex align-items-center position-relative gap-2">
-                  <div className="flex-grow-1">
-                    {key === editKey ? <input type="text" className="form-control" value={ele.taskName} onChange={e => {
-                      const dupTasks = [...tasks]
-                      dupTasks[key].taskName = e.target.value
-                      dupTasks[key].lastEdited = moment(new Date()).format("MMMM Do YYYY, h:mm:ss a")
-                      setTasks([...dupTasks])
-                    }} /> : <span className="fs-5" onDragStart={e => e.stopPropagation}>{ele.taskName}</span>}
-                  </div>
-                  <div className="d-flex align-items-center gap-3">
-                    {key === editKey ? <Check className="cursor-pointer" onClick={() => setEditKey(null)} size={17.5} /> : <Edit className="cursor-pointer" onClick={() => setEditKey(key)} size={17.5} />} <Trash className="cursor-pointer" onClick={() => {
-                      const arr = [...tasks]
-                      arr.splice(key, 1)
-                      setTasks([...arr])
-                    }} size={17.5} />
-                  </div>
-                  <span className="position-absolute cursor-pointer" style={{ inset: "0px 0px auto auto", fontSize: "10px", padding: "0.15rem" }} title={`Created at ${ele.time}${ele.lastEdited === "" ? "" : `, Last edited on ${ele.lastEdited}`}`}><Info size={12} /></span>
-                </CardBody>
-              </Card>
-            )
-          }) : (
-            <label htmlFor="enterTask" className="text-white fs-5 text-center w-100">Enter a task</label>
-          )}
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
